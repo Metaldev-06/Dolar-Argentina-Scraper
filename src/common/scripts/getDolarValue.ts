bb8e50e93aa62bf4a11b6330be50b51ccab6f594
Fernando Diaz <fernydiaz62@gmail.com>
2024-06-08 02:02:39 -0300

    Add: arguments '--no-sandbox' in puppeteer.launch

--- src/common/scripts/getDolarValue.ts
+++ src/common/scripts/getDolarValue.ts
@@ -1,31 +1,32 @@
-import puppeteer from "puppeteer";
+import puppeteer from 'puppeteer';
 
 export const extractDolarValue = async () => {
   // Launch the browser and open a new blank page
   const browser = await puppeteer.launch({
     headless: true,
     devtools: false,
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
     // slowMo: 50,
   });
   const page = await browser.newPage();
-  await page.goto("https://www.finanzasargy.com/");
+  await page.goto('https://www.finanzasargy.com/');
 
   // Esperar a que se cargue el elemento específico antes de evaluar
-  await page.waitForSelector(".chakra-card.css-12yfeo");
+  await page.waitForSelector('.chakra-card.css-12yfeo');
 
   const dolarValue = await page.evaluate(() => {
     const seen = new Set();
     const cards = document.querySelectorAll(
-      ".css-19l7epd .chakra-card.css-12yfeo"
+      '.css-19l7epd .chakra-card.css-12yfeo',
     );
     return Array.from(cards)
       .map((el) => {
-        const title = el.querySelector(".chakra-heading")?.textContent?.trim();
+        const title = el.querySelector('.chakra-heading')?.textContent?.trim();
         const sellPrice = el
-          .querySelector(".css-4xmv73 .css-umkzdf")
+          .querySelector('.css-4xmv73 .css-umkzdf')
           ?.textContent?.trim();
         const buyPrice = el
-          .querySelector(".css-4xmv73 .css-exeauf")
+          .querySelector('.css-4xmv73 .css-exeauf')
           ?.textContent?.trim();
         return {
           title,
